c6f97c11c9a02776dc8866d83349c4e1a9760c7d
Mateus Pereira Dias <mateus.pereira11@senaisp.edu.br>
2020-12-09 15:49:50 -0300

    Entity Account

---
+++ Caixa/Caixa.Business/Models/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Caixa.Business.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Holder { get; set; }
+        public string Cpf { get; set; }
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
